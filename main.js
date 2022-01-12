832b438efdcb1172c13972e154808d5269810895
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-01-12 18:35:09 -0300

    reto 8 Finalizado

--- main.js
+++ main.js
@@ -25,19 +25,27 @@ const cosasQueAprendimos = [
   },
 ];
 
-const lista = document.querySelector('ul');
+// const lista = document.querySelector('ul');
 
 // const li = document.createElement('li'); // me crea un elemento
 // li.classList.add('') // agrega una clase a ese item
 // li.innerText = 'poo' //agrega el contenido o sea el texto de esa etiqueta. el innerText es una propiedad
 
 function main() {
-  for (let i = 0; i < cosasQueAprendimos.length; i++) {
-    const li = document.createElement('li');
-    li.className = (cosasQueAprendimos[i].class);
-    li.innerText = cosasQueAprendimos[i].tema;
-    lista.appendChild(li)
-  }
+  //mi version con for:
+  // for (let i = 0; i < cosasQueAprendimos.length; i++) {
+  //   const li = document.createElement('li');
+  //   li.className = (cosasQueAprendimos[i].class);
+  //   li.innerText = cosasQueAprendimos[i].tema;
+  //   lista.appendChild(li)
+  // }
+  //la version del profe con forEach y usando classList
+  cosasQueAprendimos.forEach(cosas => {
+    const nuevoItem = document.createElement('li');
+    nuevoItem.innerText = cosas.tema;
+    cosas.class && nuevoItem.classList.add(cosas.class);
+    document.querySelector('ul.lista').appendChild(nuevoItem);
+  });
 }
 
 main();
